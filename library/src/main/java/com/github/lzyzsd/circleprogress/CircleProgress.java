48eddaba6765040ccf174548da1623ac77cdb3e5
Seyed Faridoddin Kiaei <sfdk74@gmail.com>
2019-02-13 10:35:05 +0330

    Update CircleProgress.java

--- library/src/main/java/com/github/lzyzsd/circleprogress/CircleProgress.java
+++ library/src/main/java/com/github/lzyzsd/circleprogress/CircleProgress.java
@@ -204,7 +204,7 @@ public class CircleProgress extends View {
     }
 
     public float getProgressPercentage() {
-        return (getProgress() - getMin()) / (float)(getMax() - getMin()));
+        return ((getProgress() - getMin()) / (float)(getMax() - getMin()));
     }
 
     @Override
